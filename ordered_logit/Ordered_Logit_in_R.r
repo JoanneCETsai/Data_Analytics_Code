dd1cc591e1e2586567564b84a298ec63773fc35a
Joanne Tsai <ct656@cornell.edu>
2022-11-07 12:28:53 -0500

    logit model

---
+++ ordered_logit/Ordered_Logit_in_R.r
@@ -0,0 +1,28 @@
+#ordered logistic regression
+library(rms)
+
+frame<-read.csv("LoadData3Groups.csv")
+head(frame)
+
+ologit<-lrm(Group~Credit.Borrows.Score+Debt.to.Income, data=frame)
+print(ologit)
+
+#find the probability each observation belongs to each of the three groups
+fitteddata<-predict(ologit,data=frame, type = "fitted.ind") # type = “fitted.ind” indicates that you 
+#want the output of this function to tell you the probability of each individual being in a each group
+print(fitteddata)
+frame2 <- data.frame(fitteddata)
+
+#Gain insight into the marginal impacts of each of the variables
+plot(frame$Credit.Borrows.Score, frame2$Group.1)
+plot(frame$Credit.Borrows.Score, frame2$Group.2)
+plot(frame$Credit.Borrows.Score, frame2$Group.3)
+
+plot(frame$Debt.to.Income, frame2$Group.1)
+plot(frame$Debt.to.Income, frame2$Group.2)
+plot(frame$Debt.to.Income, frame2$Group.3)
+
+#predit for a new potential customer
+mydata=c(660, 0.35)
+fitted<-predict(ologit, newdata=mydata, type="fitted.ind")
+print(fitted)
